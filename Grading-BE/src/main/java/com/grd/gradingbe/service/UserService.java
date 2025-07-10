03de99c6fb622151cedcac9b69864f09cdcdb2ca
Phan Thanh Lợi <phanthanhloi92017@gmail.com>
2025-07-10 20:57:10 +0700

    [Final] User crud
    Added update user info feature. Hide database username, password and jwt secret.

--- Grading-BE/src/main/java/com/grd/gradingbe/service/UserService.java
+++ Grading-BE/src/main/java/com/grd/gradingbe/service/UserService.java
@@ -1,6 +1,7 @@
 package com.grd.gradingbe.service;
 
 import com.grd.gradingbe.dto.request.ChangePasswordRequest;
+import com.grd.gradingbe.dto.request.UpdateUserInfoRequest;
 import com.grd.gradingbe.dto.response.UserDataResponse;
 
 import java.util.Map;
@@ -8,6 +9,8 @@ import java.util.Map;
 public interface UserService
 {
     UserDataResponse getUserData(String header);
+
+    UserDataResponse updateUserInfo (String header, UpdateUserInfoRequest request);
 
     Map<String, String> changePassword(String header, ChangePasswordRequest request);
 }
